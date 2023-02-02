d54958f0a0cfbf44bddc29397641c2eab3d989fc
Jasper Lieber <jasper.amp@gmail.com>
2023-02-02 07:14:44 +0000

    round 2:  added a configuration form & logging

--- src/EventSubscriber/DrupalSeamlessCilogonEventSubscriber.php
+++ src/EventSubscriber/DrupalSeamlessCilogonEventSubscriber.php
@@ -8,9 +8,7 @@
 namespace Drupal\drupal_seamless_cilogon\EventSubscriber;
 
 use Symfony\Component\HttpKernel\KernelEvents;
-use Symfony\Component\HttpKernel\Event\ResponseEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-
 use Symfony\Component\HttpKernel\Event\RequestEvent;
 
 /**
@@ -27,19 +25,31 @@ class DrupalSeamlessCilogonEventSubscriber implements EventSubscriberInterface {
    * checking if a non-authenticated user already has the seamless cilogon flow set.
    */
   public function onRequest(RequestEvent $event) {
+    $cookie_name = \Drupal::state()->get('drupal_seamless_cilogon.seamless_cookie_name', self::seamlessCookieName);
 
-    //  If the user is authenticated, nothing is needed and we can just return
+    $cookie_exists = isset($_COOKIE[$cookie_name]);
+
+    $seamless_debug = \Drupal::state()->get('drupal_seamless_cilogon.seamless_cookie_debug', true);
+    
+    if ($seamless_debug) {
+      $msg =  __FUNCTION__ . "() - \$_COOKIE[$cookie_name] = " 
+        . ($cookie_exists ? print_r($_COOKIE[$cookie_name], true) : ' <not set>')
+        . ' -- ' . basename(__FILE__) . ':' . __LINE__ ;  
+      \Drupal::messenger()->addStatus($msg);
+      \Drupal::logger('seamless_cilogon')->debug($msg);
+    }
+
+
+    //  If the user is authenticated,no need to redirect to CILogin
     if (\Drupal::currentUser()->isAuthenticated()) {
       return;
     }
 
+    // Don't attempt to redirect if the cilogon_auth module is not installed
     $moduleHandler = \Drupal::service('module_handler');
     if (!$moduleHandler->moduleExists('cilogon_auth')) {
       return;
     }
-
-    // 
-    $cookie_exists = isset($_COOKIE[self::seamlessCookieName]);
 
     // if cookie is set, redirect to CILogon flow
     // if no cookie, do nothing, just return
@@ -66,10 +76,25 @@ class DrupalSeamlessCilogonEventSubscriber implements EventSubscriberInterface {
 
     $response = $client->authorize($scopes);
 
-    $event->setResponse($response);
+    $seamless_login_enabled = \Drupal::state()->get('drupal_seamless_cilogon.seamless_login_enabled', true);
+    
+    if ($seamless_debug) {
+      $msg =  __FUNCTION__ . "() - cookie exists, redirecting to cilogon is "
+        . ($seamless_login_enabled ? "ENABLED" : "DISABLED")
+        . ' -- ' . basename(__FILE__) . ':' . __LINE__ ;  
+      \Drupal::messenger()->addStatus($msg);
+      \Drupal::logger('seamless_cilogon')->debug($msg);
+    }
+
+    if ($seamless_login_enabled) {
+      $event->setResponse($response);
+    }
   }
 
   /**
+   * Subscribe to onRequest events.  This allows checking if a CILogon redirect is needed any time 
+   * a page is requested.
+   * 
    * {@inheritdoc}
    */
   public static function getSubscribedEvents() {
---
+++ src/Form/DrupalSeamlessCilogon.php
@@ -0,0 +1,137 @@
+<?php
+
+namespace Drupal\drupal_seamless_cilogon\Form;
+
+use Drupal\drupal_seamless_cilogon\EventSubscriber\DrupalSeamlessCilogonEventSubscriber;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Form\FormBase;
+
+/**
+ * Class DrupalSeamlessCilogon
+ */  
+class DrupalSeamlessCilogon extends FormBase
+{
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state)
+  {
+
+    $seamless_login_enabled = \Drupal::state()->get('drupal_seamless_cilogon.seamless_login_enabled', true);
+
+    $cookie_name = \Drupal::state()->get(
+      'drupal_seamless_cilogon.seamless_cookie_name',
+      DrupalSeamlessCilogonEventSubscriber::seamlessCookieName
+    );
+
+    $site_name = \Drupal::config('system.site')->get('name');
+    $cookie_value = \Drupal::state()->get('drupal_seamless_cilogon.seamless_cookie_value', "INITIAL_DOMAIN=$site_name");
+
+    $cookie_expiration = \Drupal::state()->get('drupal_seamless_cilogon.seamless_cookie_expiration', '+18 hours');
+
+    $seamless_debug = \Drupal::state()->get('drupal_seamless_cilogon.seamless_cookie_debug', true);
+
+    $form['seamless_login_enabled'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Enable seamless redirect to CILogon?'),
+      '#description' => $this->t('Disable this for testing.'),
+      '#default_value' => $seamless_login_enabled,
+    ];
+
+    $form['seamless_cookie_name'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Seamless CILogin - cookie name'),
+      '#maxlength' => 255,
+      '#default_value' => $cookie_name,
+      '#description' => $this->t("Name for the seamless login cookie.  Default value is " 
+        . DrupalSeamlessCilogonEventSubscriber::seamlessCookieName),
+      '#required' => false,
+    ];
+
+    $form['seamless_cookie_value'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Cookie value'),
+      '#maxlength' => 255,
+      '#default_value' => $cookie_value,
+      '#description' => $this->t("Value for the cookie."),
+      '#required' => false,
+    ];
+
+    $form['seamless_cookie_expiration'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Cookie expiration (as argument to strtotime()'),
+      '#maxlength' => 255,
+      '#default_value' => $cookie_expiration,
+      '#description' => $this->t('Example:  "+18 hours" sets expiration to 18 hours from now')
+    ];
+
+    $form['seamless_cookie_debug'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Enable debug logging?'),
+      '#description' => $this->t('Logging will go to screen when possible, and to drupal log with label "seamless_cilogon"'),
+      '#default_value' => $seamless_debug,
+    ];
+
+    $form['save_seamless_settings'] = [
+      '#type' => 'submit',
+      '#value' => $this->t('Save Seamless CILogon Settings'),
+      '#submit' => [[$this, 'doSaveSeamlessSettings']],
+    ];
+    return $form;
+  }
+
+  /**
+   * Getter method for Form ID.
+   *
+   * @return string
+   *   The unique ID of the form defined by this class.
+   */
+  public function getFormId()
+  {
+    return 'drupal_seamless_cilogon_form';
+  }
+
+  // TODO -- Implements any form validation?  Maybe especially for the cookie expiration ?
+  
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state)
+  {
+    parent::submitForm($form, $form_state);
+  }
+
+  /**
+   *
+   */
+  public function doSaveSeamlessSettings(array &$form, FormStateInterface $form_state)
+  {
+    \Drupal::state()->set('drupal_seamless_cilogon.seamless_login_enabled', $form_state->getValue('seamless_login_enabled'));
+    \Drupal::state()->set('drupal_seamless_cilogon.seamless_cookie_name', $form_state->getValue('seamless_cookie_name'));
+    \Drupal::state()->set('drupal_seamless_cilogon.seamless_cookie_value', $form_state->getValue('seamless_cookie_value'));
+    \Drupal::state()->set('drupal_seamless_cilogon.seamless_cookie_expiration', $form_state->getValue('seamless_cookie_expiration'));
+
+    $seamless_debug = $form_state->getValue('seamless_cookie_debug');
+    \Drupal::state()->set('drupal_seamless_cilogon.seamless_cookie_debug', $seamless_debug);
+
+    if ($seamless_debug) {
+      
+      $seamless_login_enabled = \Drupal::state()->get('drupal_seamless_cilogon.seamless_login_enabled', true);
+      $cookie_name = \Drupal::state()->get(
+        'drupal_seamless_cilogon.seamless_cookie_name',
+        DrupalSeamlessCilogonEventSubscriber::seamlessCookieName
+      );
+      $site_name = \Drupal::config('system.site')->get('name');
+      $cookie_value = \Drupal::state()->get('drupal_seamless_cilogon.seamless_cookie_value', "INITIAL_DOMAIN=$site_name");
+      $cookie_expiration = \Drupal::state()->get('drupal_seamless_cilogon.seamless_cookie_expiration', '+18 hours');
+      $seamless_debug = \Drupal::state()->get('drupal_seamless_cilogon.seamless_cookie_debug', true);
+
+      $msg =  __FUNCTION__ . "(): seamless_login_enabled=$seamless_login_enabled cookie_name=$cookie_name cookie_value=$cookie_value cookie_expiration=$cookie_expiration seamless_debug=$seamless_debug"
+        . ' -- ' . basename(__FILE__) . ':' . __LINE__ ;  
+      \Drupal::messenger()->addStatus($msg);
+      \Drupal::logger('seamless_cilogon')->debug($msg);
+    }
+
+  }
+}
