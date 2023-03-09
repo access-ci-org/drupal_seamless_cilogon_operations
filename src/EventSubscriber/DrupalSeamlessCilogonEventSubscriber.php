990f008a886eb65daf830c052cc32893be6ddc55
Jasper Lieber <jasper.amp@gmail.com>
2023-03-09 19:53:51 +0000

    setting destination on login, cookie name back to 'access_ci_sso', formatting

--- src/EventSubscriber/DrupalSeamlessCilogonEventSubscriber.php
+++ src/EventSubscriber/DrupalSeamlessCilogonEventSubscriber.php
@@ -12,20 +12,22 @@ use Symfony\Component\HttpFoundation\Cookie;
 /**
  * Event Subscriber DrupalSeamlessCilogonEventSubscriber.
  */
-class DrupalSeamlessCilogonEventSubscriber implements EventSubscriberInterface {
+class DrupalSeamlessCilogonEventSubscriber implements EventSubscriberInterface
+{
 
   // for pantheon, cookie name must follow pattern S+ESS[a-z0-9]+
   // (see https://docs.pantheon.io/cookies#cache-busting-cookies)
-  const SEAMLESSCOOKIENAME = 'SESSaccesscisso';
+  const SEAMLESSCOOKIENAME = 'access_ci_sso'; //'SESSaccesscisso';
 
   /**
    * Event handler for KernelEvents::REQUEST events, specifically to support
    * seamless login by checking if a non-authenticated user already has already
    * been through seamless login.
    */
-  public function onRequest(RequestEvent $event) {
+  public function onRequest(RequestEvent $event)
+  {
 
-    if ( !$event->isMasterRequest()) {
+    if (!$event->isMasterRequest()) {
       return;
     }
 
@@ -37,7 +39,7 @@ class DrupalSeamlessCilogonEventSubscriber implements EventSubscriberInterface {
     if (!$seamless_login_enabled) {
       return;
     }
-    
+
     // Don't attempt to redirect if the cilogon_auth module is not installed.
     $moduleHandler = \Drupal::service('module_handler');
     if (!$moduleHandler->moduleExists('cilogon_auth')) {
@@ -63,28 +65,30 @@ class DrupalSeamlessCilogonEventSubscriber implements EventSubscriberInterface {
 
     // if coming back from cilogon, set the cookie
     if ($route_name === 'cilogon_auth.redirect_controller_redirect') {
-      if (!$cookie_exists) { 
+      if (!$cookie_exists) {
         $this->doSetCookie($event, $seamless_debug, $cookie_name);
       }
       return;
-    } 
+    }
 
     // if logging out, delete the cookie
     if ($route_name === 'user.logout') {
-      if ($cookie_exists) { 
+      if ($cookie_exists) {
         $this->doDeleteCookie($event, $seamless_debug, $cookie_name);
       }
       return;
-    } 
+    }
 
     // If the user is authenticated, no need to redirect to CILogin, unless cookie doesn't exist, in 
     // which case, logout
     if ($user_is_authenticated) {
       // Unless cookie doesn't exist. In this case, logout.
-      if (!$cookie_exists && 
-          $route_name !== 'user.logout' && 
-          $route_name !== 'user.login') {
-            
+      if (
+        !$cookie_exists &&
+        $route_name !== 'user.logout' &&
+        $route_name !== 'user.login'
+      ) {
+
         if ($seamless_debug) {
           $msg = __FUNCTION__ . "() - user authenticated but no cookie found, so redirect to /user/logout"
             . ' -- ' . basename(__FILE__) . ':' . __LINE__;
@@ -97,14 +101,13 @@ class DrupalSeamlessCilogonEventSubscriber implements EventSubscriberInterface {
         $redir->headers->set('Cache-Control', 'public, max-age=0');
         $redir->addCacheableDependency($destination);
         $event->setResponse($redir);
-
       }
       return;
     }
 
     // if here -- user is unauthenticated.  If cookie exists, redirect to cilogon
     if ($cookie_exists) {
-        $this->doRedirectToCilogon($event, $seamless_debug);
+      $this->doRedirectToCilogon($event, $seamless_debug);
     }
   }
 
@@ -115,7 +118,8 @@ class DrupalSeamlessCilogonEventSubscriber implements EventSubscriberInterface {
    *   Response event.
 
    */
-  protected function doSetCookie(RequestEvent $event, $seamless_debug, $cookie_name) {
+  protected function doSetCookie(RequestEvent $event, $seamless_debug, $cookie_name)
+  {
 
     $site_name = \Drupal::config('system.site')->get('name');
     $cookie_value = \Drupal::state()->get('drupal_seamless_cilogon.seamless_cookie_value', $site_name);
@@ -134,7 +138,7 @@ class DrupalSeamlessCilogonEventSubscriber implements EventSubscriberInterface {
     // from https://www.drupal.org/project/adv_varnish/issues/3127566:
     // Another documented way is to call the killSwitch in your code:
     \Drupal::service('page_cache_kill_switch')->trigger();
-    
+
     $redir = new TrustedRedirectResponse($destination, '302');
     $redir->headers->setCookie($cookie);
     $redir->headers->set('Cache-Control', 'public, max-age=0');
@@ -146,7 +150,7 @@ class DrupalSeamlessCilogonEventSubscriber implements EventSubscriberInterface {
     if ($seamless_debug) {
       $msg =  __FUNCTION__ . "() - destination = $destination ---- set cookie:  name = $cookie_name, value = $cookie_value, expiration = $cookie_expiration "
         . " = " . date("Y-m-d H:i:s", $cookie_expiration) . ", domain = $cookie_domain"
-        . ' -- ' . basename(__FILE__) . ':' . __LINE__ ;
+        . ' -- ' . basename(__FILE__) . ':' . __LINE__;
       \Drupal::messenger()->addStatus($msg);
       \Drupal::logger('seamless_cilogon')->debug($msg);
     }
@@ -159,10 +163,11 @@ class DrupalSeamlessCilogonEventSubscriber implements EventSubscriberInterface {
    *   Response event.
 
    */
-  protected function doDeleteCookie(RequestEvent $event, $seamless_debug, $cookie_name) {
+  protected function doDeleteCookie(RequestEvent $event, $seamless_debug, $cookie_name)
+  {
 
     $cookie_value = '';
-    $cookie_expiration = strtotime('-1 hour'); 
+    $cookie_expiration = strtotime('-1 hour');
     $cookie_domain = \Drupal::state()->get('drupal_seamless_cilogon.seamless_cookie_domain', '.access-ci.org');
 
     // Set cookie in the past and then remove it.
@@ -170,7 +175,7 @@ class DrupalSeamlessCilogonEventSubscriber implements EventSubscriberInterface {
     unset($_COOKIE[$cookie_name]);
 
     // $request = $event->getRequest();
-    $destination = 'https://cilogon.org/logout/?skin=access'; 
+    $destination = 'https://cilogon.org/logout/?skin=access';
     // $destination = $request->getRequestUri();
 
     // TODO -- consider following
@@ -180,7 +185,7 @@ class DrupalSeamlessCilogonEventSubscriber implements EventSubscriberInterface {
     // from https://www.drupal.org/project/adv_varnish/issues/3127566:
     // Another documented way is to call the killSwitch in your code:
     \Drupal::service('page_cache_kill_switch')->trigger();
-    
+
     $redir = new TrustedRedirectResponse($destination, '302');
     $redir->headers->set('Cache-Control', 'public, max-age=0');
     $redir->addCacheableDependency($destination);
@@ -189,7 +194,7 @@ class DrupalSeamlessCilogonEventSubscriber implements EventSubscriberInterface {
 
     if ($seamless_debug) {
       $msg =  __FUNCTION__ . "() - destination = $destination ---- unset cookie"
-        . ' -- ' . basename(__FILE__) . ':' . __LINE__ ;
+        . ' -- ' . basename(__FILE__) . ':' . __LINE__;
       \Drupal::messenger()->addStatus($msg);
       \Drupal::logger('seamless_cilogon')->debug($msg);
       error_log('seamless: ' . $msg);
@@ -203,12 +208,9 @@ class DrupalSeamlessCilogonEventSubscriber implements EventSubscriberInterface {
    *   Response event.
 
    */
-  protected function doRedirectToCilogon(RequestEvent $event, $seamless_debug) {
-
+  protected function doRedirectToCilogon(RequestEvent $event, $seamless_debug)
+  {
     $request = $event->getRequest();
-
-    // TODO make use of the destination somehow?  for after return from redirect?
-    $destination = $request->getRequestUri();
 
     // TODO -- consider following
     // "MUST use service to turn of Internal Page Cache,
@@ -217,9 +219,9 @@ class DrupalSeamlessCilogonEventSubscriber implements EventSubscriberInterface {
     // from https://www.drupal.org/project/adv_varnish/issues/3127566:
     // Another documented way is to call the killSwitch in your code:
     \Drupal::service('page_cache_kill_switch')->trigger();
-    
+
     // Setup redirect to CILogon flow.
-    // TODO - move some of the following be moved to a constructor for this class?
+    // TODO - move some of the following to a constructor for this class?
     $container = \Drupal::getContainer();
     $client_name = 'cilogon';
     $config_name = 'cilogon_auth.settings.' . $client_name;
@@ -228,7 +230,11 @@ class DrupalSeamlessCilogonEventSubscriber implements EventSubscriberInterface {
     $claims = $container->get('cilogon_auth.claims');
     $client = $pluginManager->createInstance($client_name, $configuration);
     $scopes = $claims->getScopes();
+    $destination = $request->getRequestUri();
+    $query = $request->getQueryString();
     $_SESSION['cilogon_auth_op'] = 'login';
+    $_SESSION['cilogon_auth_destination'] = [$destination, ['query' => $query]];
+
     $response = $client->authorize($scopes);
     $response->headers->set('Cache-Control', 'public, max-age=0');
 
@@ -240,13 +246,13 @@ class DrupalSeamlessCilogonEventSubscriber implements EventSubscriberInterface {
 
     if ($seamless_debug) {
       $msg =  __FUNCTION__ . "() - destination = $destination ---- "
-        . ' -- ' . basename(__FILE__) . ':' . __LINE__ ;
+        . ' -- ' . basename(__FILE__) . ':' . __LINE__;
       \Drupal::messenger()->addStatus($msg);
       \Drupal::logger('seamless_cilogon')->debug($msg);
     }
   }
 
-  
+
   /**
    * The ACCESS support portal uses the domain access module.  If this module
    * is in use, we only want to set cookies for the 'access-support'
@@ -277,9 +283,9 @@ class DrupalSeamlessCilogonEventSubscriber implements EventSubscriberInterface {
 
     $seamless_debug = \Drupal::state()->get('drupal_seamless_cilogon.seamless_cookie_debug', false);
     if ($seamless_debug) {
-      $msg =  __FUNCTION__ . "() - current_domain_name = [" . $current_domain_name 
+      $msg =  __FUNCTION__ . "() - current_domain_name = [" . $current_domain_name
         . '] so verify_domain_is_asp() returns ' . ($domain_verified ? 'TRUE' : 'FALSE')
-        . ' -- ' . basename(__FILE__) . ':' . __LINE__ ;
+        . ' -- ' . basename(__FILE__) . ':' . __LINE__;
       \Drupal::logger('seamless_cilogon')->debug($msg);
       \Drupal::messenger()->addStatus($msg);
     }
@@ -294,7 +300,8 @@ class DrupalSeamlessCilogonEventSubscriber implements EventSubscriberInterface {
    *
    * {@inheritdoc}
    */
-  public static function getSubscribedEvents() {
+  public static function getSubscribedEvents()
+  {
     $events[KernelEvents::REQUEST][] = ['onRequest', 31];
     return $events;
   }
