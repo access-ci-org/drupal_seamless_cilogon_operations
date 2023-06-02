b1b24abbc3fff113647748625a3f7f5f5d02a7fb
protitude <miles.france@gmail.com>
2023-06-02 11:42:31 -0600

    CookieMiddleware: lots of tweaks d8-1501

--- src/StackMiddleware/CookieMiddleware.php
+++ src/StackMiddleware/CookieMiddleware.php
@@ -2,8 +2,11 @@
 
 namespace Drupal\drupal_seamless_cilogon\StackMiddleware;
 
+use Drupal\Core\Routing\TrustedRedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
+use Symfony\Component\HttpFoundation\Cookie;
 
 /**
  * Provides a HTTP middleware.
@@ -18,7 +21,7 @@ class CookieMiddleware implements HttpKernelInterface {
   protected $httpKernel;
 
   /**
-   * Constructs a MyModule object.
+   * Constructs a CookieMiddleware object.
    *
    * @param \Symfony\Component\HttpKernel\HttpKernelInterface $kernel
    *   The decorated kernel.
@@ -33,29 +36,34 @@ class CookieMiddleware implements HttpKernelInterface {
    * {@inheritdoc}
    */
   public function handle(Request $request, $type = self::MASTER_REQUEST, $catch = TRUE) {
-    if (!$event->isMasterRequest()) {
+    if (!$type) {
+      \Drupal::logger('drupal_seamless_login')->notice('type');
       return $this->httpKernel->handle($request, $type, $catch);
     }
 
     if (!$this->verify_domain_is_asp()) {
+      \Drupal::logger('drupal_seamless_login')->notice('domain');
       return $this->httpKernel->handle($request, $type, $catch);
     }
 
     $seamless_login_enabled = \Drupal::state()->get('drupal_seamless_cilogon.seamless_login_enabled', TRUE);
     if (!$seamless_login_enabled) {
+      \Drupal::logger('drupal_seamless_login')->notice('seamless');
       return $this->httpKernel->handle($request, $type, $catch);
     }
 
     // Don't attempt to redirect if the cilogon_auth module is not installed.
     $moduleHandler = \Drupal::service('module_handler');
     if (!$moduleHandler->moduleExists('cilogon_auth')) {
+      \Drupal::logger('drupal_seamless_login')->notice('cilogin_auth');
       return $this->httpKernel->handle($request, $type, $catch);
     }
 
     $user_is_authenticated = \Drupal::currentUser()->isAuthenticated();
-    $route_name = \Drupal::routeMatch()->getRouteName();
-    $cookie_name = self::SEAMLESSCOOKIENAME;
-    $cookie_exists = NULL !== \Drupal::service('request_stack')->getCurrentRequest()->cookies->get($cookie_name);
+    $path = $_SERVER['REQUEST_URI'];
+    $arg = explode('/', $path);
+    $cookie_name = $_COOKIE['SESSaccesscisso'];
+    $cookie_exists = NULL !== $cookie_name;
     $seamless_debug = \Drupal::state()->get('drupal_seamless_cilogon.seamless_cookie_debug', FALSE);
 
     if ($seamless_debug) {
@@ -69,9 +77,10 @@ class CookieMiddleware implements HttpKernelInterface {
     }
 
     // If coming back from cilogon, set the cookie.
-    if ($route_name === 'cilogon_auth.redirect_controller_redirect') {
+    if ($arg[1] === 'cilogon-auth') {
+      \Drupal::logger('drupal_seamless_login')->notice('cilogin');
       if (!$cookie_exists) {
-        $this->doSetCookie($event, $seamless_debug, $cookie_name);
+        $this->doSetCookie($uri, $seamless_debug, $cookie_name);
       }
       return $this->httpKernel->handle($request, $type, $catch);
     }
@@ -79,7 +88,7 @@ class CookieMiddleware implements HttpKernelInterface {
     // If logging out, delete the cookie.
     if ($route_name === 'user.logout') {
       if ($cookie_exists) {
-        $this->doDeleteCookie($event, $seamless_debug, $cookie_name);
+        $this->doDeleteCookie($uri, $seamless_debug, $cookie_name);
       }
       return $this->httpKernel->handle($request, $type, $catch);
     }
@@ -97,14 +106,62 @@ class CookieMiddleware implements HttpKernelInterface {
         $redir = new TrustedRedirectResponse($destination, '302');
         $redir->headers->set('Cache-Control', 'public, max-age=0');
         $redir->addCacheableDependency($destination);
-        $event->setResponse($redir);
+        // $event->setResponse($redir);
       }
       return $this->httpKernel->handle($request, $type, $catch);
     }
 
     // If here -- user is unauthenticated.  If cookie exists, redirect to cilogon.
     if ($cookie_exists) {
-      $this->doRedirectToCilogon($event, $seamless_debug);
+      \Drupal::logger('drupal_seamless_login')->notice('bing');
+      $this->doRedirectToCilogon($request, $seamless_debug);
+    }
+
+    return $this->httpKernel->handle($request, $type, $catch);
+
+  }
+
+  /**
+   * Add the cookie, via a redirect.
+   *
+   * @param \Symfony\Component\HttpKernel\Event\RequestEvent $event
+   *   Response event.   *.
+   */
+  protected function doSetCookie($uri, $seamless_debug, $cookie_name) {
+    $event = new RequestEvent();
+
+    $site_name = \Drupal::config('system.site')->get('name');
+    $cookie_value = \Drupal::state()->get('drupal_seamless_cilogon.seamless_cookie_value', $site_name);
+    $cookie_expiration = \Drupal::state()->get('drupal_seamless_cilogon.seamless_cookie_expiration', '+18 hours');
+    // Use value from form.
+    $cookie_expiration = strtotime($cookie_expiration);
+    $cookie_domain = \Drupal::state()->get('drupal_seamless_cilogon.seamless_cookie_domain', '.access-ci.org');
+    $cookie = new Cookie($cookie_name, $cookie_value, $cookie_expiration, '/', $cookie_domain);
+
+    $destination = $uri;
+
+    // @todo consider following
+    // "MUST use service to turn of Internal Page Cache,
+    // or else anonymous users will not ever be able to reach source page."
+    // $this->killSwitch->trigger();
+    // from https://www.drupal.org/project/adv_varnish/issues/3127566:
+    // Another documented way is to call the killSwitch in your code:
+    //
+    // commenting this out to see unnecessary
+    // \Drupal::service('page_cache_kill_switch')->trigger();
+    $redir = new TrustedRedirectResponse($destination, '302');
+    $redir->headers->setCookie($cookie);
+    $redir->headers->set('Cache-Control', 'public, max-age=0');
+    $redir->addCacheableDependency($destination);
+    $redir->addCacheableDependency($cookie);
+
+    $event->setResponse($redir);
+
+    if ($seamless_debug) {
+      $msg = __FUNCTION__ . "() - destination = $destination ---- set cookie:  name = $cookie_name, value = $cookie_value, expiration = $cookie_expiration "
+        . " = " . date("Y-m-d H:i:s", $cookie_expiration) . ", domain = $cookie_domain"
+        . ' -- ' . basename(__FILE__) . ':' . __LINE__;
+      \Drupal::messenger()->addStatus($msg);
     }
   }
 
@@ -114,8 +171,7 @@ class CookieMiddleware implements HttpKernelInterface {
    * @param \Symfony\Component\HttpKernel\Event\RequestEvent $event
    *   Response event.   *.
    */
-  protected function doRedirectToCilogon(RequestEvent $event, $seamless_debug) {
-    $request = $event->getRequest();
+  protected function doRedirectToCilogon(Request $request, $seamless_debug) {
 
     // \Drupal::service('page_cache_kill_switch')->trigger();
     // Setup redirect to CILogon flow.
@@ -136,8 +192,10 @@ class CookieMiddleware implements HttpKernelInterface {
     $response = $client->authorize($scopes);
     $response->headers->set('Cache-Control', 'public, max-age=0');
 
-    $event->setResponse($response);
+    new TrustedRedirectResponse($response->getTargetUrl(), 301);
+    return $this->httpKernel->handle($request, $type, $catch);
 
+    // $event->setResponse($response);
     if ($seamless_debug) {
       $msg = __FUNCTION__ . "() - destination = $destination ---- "
         . ' -- ' . basename(__FILE__) . ':' . __LINE__;
@@ -165,11 +223,10 @@ class CookieMiddleware implements HttpKernelInterface {
       return TRUE;
     }
 
-    $token = \Drupal::token();
-    $domainName = t("[domain:name]");
-    $current_domain_name = Html::getClass($token->replace($domainName));
+    $domain_storage = \Drupal::entityTypeManager()->getStorage('domain');
+    $current_domain_name = $domain_storage->loadDefaultId();
 
-    $domain_verified = $current_domain_name === 'access-support';
+    $domain_verified = $current_domain_name === 'amp_cyberinfrastructure_org';
 
     $seamless_debug = \Drupal::state()->get('drupal_seamless_cilogon.seamless_cookie_debug', FALSE);
     if ($seamless_debug) {
