b9f43f8c8fe2fe585d048e6ab6aa7d0b8e6a59fa
protitude <miles.france@gmail.com>
2023-06-22 11:17:49 -0600

    logging: Add logging to middleware and add to form to controll d8-1583

--- src/StackMiddleware/CookieMiddleware.php
+++ src/StackMiddleware/CookieMiddleware.php
@@ -4,7 +4,9 @@ namespace Drupal\drupal_seamless_cilogon\StackMiddleware;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
+use Drupal\Core\Logger\LoggerChannelFactoryInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Drupal\Core\State\StateInterface;
 
 /**
  * Provides a HTTP middleware.
@@ -19,68 +21,115 @@ class CookieMiddleware implements HttpKernelInterface {
   protected $httpKernel;
 
   /**
+   * The RedirectAddAnalytics logging channel.
+   *
+   * @var \Drupal\Core\Logger\LoggerChannelInterface
+   */
+  protected $logger;
+
+  /**
+   * The state key/value store.
+   *
+   * @var \Drupal\Core\State\StateInterface
+   */
+  protected $state;
+
+  /**
    * Constructs a drupal_seamless_cilogin object.
    *
    * @param \Symfony\Component\HttpKernel\HttpKernelInterface $kernel
    *   The decorated kernel.
-   * @param mixed $optional_argument
-   *   (optional) An optional argument.
+   * @param \Drupal\Core\Logger\LoggerChannelFactoryInterface $config_factory
+   *   Logging interface.
+   * @param \Drupal\Core\State\StateInterface $state
+   *   The state key/value store.
    */
-  public function __construct(HttpKernelInterface $http_kernel) {
+  public function __construct(
+    HttpKernelInterface $http_kernel,
+    LoggerChannelFactoryInterface $channelFactory,
+    StateInterface $state
+  ) {
     $this->httpKernel = $http_kernel;
+    $this->logger = $channelFactory->get('drupal_seamless_cilogon');
+    $this->state = $state;
   }
 
   /**
    * {@inheritdoc}
    */
   public function handle(Request $request, $type = self::MASTER_REQUEST, $catch = TRUE) {
+    $logging = $this->state->get('drupal_seamless_cilogon.logging');
     if (!$type) {
+      if ($logging) {
+        $this->logger->notice('type');
+      }
       return $this->httpKernel->handle($request, $type, $catch);
     }
 
     if (!$this->verify_domain_is_asp()) {
+      if ($logging) {
+        $this->logger->notice('verify_domain_is_asp');
+      }
       return $this->httpKernel->handle($request, $type, $catch);
     }
 
     $seamless_login_enabled = \Drupal::state()->get('drupal_seamless_cilogon.seamless_login_enabled', TRUE);
     if (!$seamless_login_enabled) {
+      if ($logging) {
+        $this->logger->notice('seamless_login_enabled');
+      }
       return $this->httpKernel->handle($request, $type, $catch);
     }
 
     // Don't attempt to redirect if the cilogon_auth module is not installed.
     $moduleHandler = \Drupal::service('module_handler');
     if (!$moduleHandler->moduleExists('cilogon_auth')) {
+      if ($logging) {
+        $this->logger->notice('module exists cilogon_auth');
+      }
       return $this->httpKernel->handle($request, $type, $catch);
     }
 
     $user_is_authenticated = isset($_SERVER['HTTP_COOKIE']) ? TRUE : FALSE;
     $path = $request->getRequestUri();
     $arg = explode('/', $path);
-    $cookie_name = isset($_COOKIE['SESSaccesscisso']) ? $_COOKIE['SESSaccesscisso'] : NULL;
+    $cookie_name = $_COOKIE['SESSaccesscisso'] ?? NULL;
     $cookie_exists = NULL !== $cookie_name;
 
     if (str_starts_with($arg[1], 'user')) {
+      if ($logging) {
+        $this->logger->notice('/path: user');
+      }
       return $this->httpKernel->handle($request, $type, $catch);
     }
 
     // If coming back from cilogon, set the cookie.
-    if ($arg[1] === 'cilogon-auth') {
+    if ($arg[1] === 'path: cilogon-auth') {
+      if ($logging) {
+        $this->logger->notice('/cilogin-auth');
+      }
       return $this->httpKernel->handle($request, $type, $catch);
     }
 
     // If hitting the nc page, not cached and will pass.
     if ($arg[1] === 'nc') {
+      if ($logging) {
+        $this->logger->notice('path: /nc');
+      }
       return $this->httpKernel->handle($request, $type, $catch);
     }
 
     // If the user is authenticated, no need to redirect to CILogon, unless cookie doesn't exist, in
-    // which case, logout
+    // which case, logout.
     if ($user_is_authenticated) {
       // Unless cookie doesn't exist. In this case, logout.
       if (
         !$cookie_exists &&
         str_starts_with($arg[1], 'user')
       ) {
+        if ($logging) {
+          $this->logger->notice('redirect /user/logout');
+        }
         return new RedirectResponse($request->getBasePath() . "/user/logout", 302, ['Cache-Control' => 'no-cache']);
       }
       return $this->httpKernel->handle($request, $type, $catch);
@@ -88,6 +137,9 @@ class CookieMiddleware implements HttpKernelInterface {
 
     // If here -- user is unauthenticated.  If cookie exists, redirect to cilogon.
     if ($cookie_exists) {
+      if ($logging) {
+        $this->logger->notice('redirect /user?redirect=path');
+      }
       $from = $request->getRequestUri();
       return new RedirectResponse($request->getBasePath() . "/user?redirect=$from", 302, ['Cache-Control' => 'no-cache']);
     }
@@ -108,7 +160,7 @@ class CookieMiddleware implements HttpKernelInterface {
    * @return bool whether to proceed with the cookie logic in invoking code
    */
   protected function verify_domain_is_asp() {
- // Verify the domain module is installed.  If not installed,
+    // Verify the domain module is installed.  If not installed,
     // return true to proceed to CILogon.
     $moduleHandler = \Drupal::service('module_handler');
     if (!$moduleHandler->moduleExists('domain')) {
